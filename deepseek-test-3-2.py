e9666f396e5fb0d3b457fa93ea53b9129482a35f
tulayang <itulayangi@outlook.com>
2025-07-02 18:09:24 +0800

    Add sklearn-1

--- deepseek-test-3-2.py
+++ deepseek-test-3-2.py
@@ -19,38 +19,44 @@ async def main():
     temperature=0.7,
     max_tokens=1024
   )
-
-  reasoning_content = ""
+  
   content = ""
-
-  # 当 stream=True 时，openai 似乎不能正确解析 reasoning_content
-  # 所以，我们只好手动解析 - 通过一个迷你状态机
-
-  STATE_INIT = 1
-  STATE_REASON = 2
-  STATE_CONTENT = 3
-
-  # 实时处理流式响应
-  # 解析对话
-  state = STATE_INIT
   async for chunk in response:
-    if hasattr(chunk.choices[0].delta, "content"):
-      token = chunk.choices[0].delta.content
-      if state == STATE_INIT:
-        if token == '<think>':
-          state = STATE_REASON
-        elif not re.match(r"^([\s\r\n]*)$", token):
-          state = STATE_CONTENT
-          header_content += token
-      elif state == STATE_REASON:
-        if token == '</think>':
-          state = STATE_CONTENT
-        else:
-          reasoning_content += token
-          print(f'reasoning_content: {reasoning_content}')
-      elif state == STATE_CONTENT:
-        content += token
-        print(f'content: {content}')
+    chunk_content = chunk.choices[0].delta.content or ""
+    content += chunk_content
+    print(chunk_content, end="", flush=True) 
+
+  # reasoning_content = ""
+  # content = ""
+
+  # # 当 stream=True 时，openai 似乎不能正确解析 reasoning_content
+  # # 所以，我们只好手动解析 - 通过一个迷你状态机
+
+  # STATE_INIT = 1
+  # STATE_REASON = 2
+  # STATE_CONTENT = 3
+
+  # # 实时处理流式响应
+  # # 解析对话
+  # state = STATE_INIT
+  # async for chunk in response:
+  #   if hasattr(chunk.choices[0].delta, "content"):
+  #     token = chunk.choices[0].delta.content
+  #     if state == STATE_INIT:
+  #       if token == '<think>':
+  #         state = STATE_REASON
+  #       elif not re.match(r"^([\s\r\n]*)$", token):
+  #         state = STATE_CONTENT
+  #         header_content += token
+  #     elif state == STATE_REASON:
+  #       if token == '</think>':
+  #         state = STATE_CONTENT
+  #       else:
+  #         reasoning_content += token
+  #         print(f'reasoning_content: {reasoning_content}')
+  #     elif state == STATE_CONTENT:
+  #       content += token
+  #       print(f'content: {content}')
 
   print('=================================================')
 
